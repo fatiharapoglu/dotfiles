98401385c023be18e62d62c28d37484f6043549f
Fatih Arapoglu <f3tt4n@gmail.com>
2023-04-04 18:43:33 +0300

    add tailwind and next dotfiles

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// @type {import ("tailwindcss").Config}
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            blue: "#1fb6ff",
+            purple: "#7e5bef",
+            pink: "#ff49db",
+        },
+        fontFamily: {
+            montserrat: ["var(--font-1)"],
+        },
+        extend: {
+            spacing: {
+                "8xl": "96rem",
+                "9xl": "128rem",
+            },
+            borderRadius: {
+                "4xl": "2rem",
+            },
+        },
+    },
+};
